a3b7676c3253334e5f47ec0c43dc2d58ddf15b3c
Tyler Lowe <99204234+TylerRLowe@users.noreply.github.com>
2023-07-01 17:46:29 -0400

    Add files via upload

---
+++ chess 2.0/classes/pieces/Bishop.py
@@ -0,0 +1,44 @@
+import pygame
+from classes.pieces.Piece import Piece
+
+class Bishop(Piece):
+    def __init__(self, pos, color, board):
+        self.name = "Bishop"
+        super().__init__(pos, color, board)
+        self.notation = 'B'
+
+    def get_possible_moves(self, board):
+        output = []
+        moves_ne = []
+        for i in range(1, 8):
+            if self.x + i > 7 or self.y - i < 0:
+                break
+            moves_ne.append(board.get_square_from_pos(
+                (self.x + i, self.y - i)
+            ))
+        output.append(moves_ne)
+        moves_se = []
+        for i in range(1, 8):
+            if self.x + i > 7 or self.y + i > 7:
+                break
+            moves_se.append(board.get_square_from_pos(
+                (self.x + i, self.y + i)
+            ))
+        output.append(moves_se)
+        moves_sw = []
+        for i in range(1, 8):
+            if self.x - i < 0 or self.y + i > 7:
+                break
+            moves_sw.append(board.get_square_from_pos(
+                (self.x - i, self.y + i)
+            ))
+        output.append(moves_sw)
+        moves_nw = []
+        for i in range(1, 8):
+            if self.x - i < 0 or self.y - i < 0:
+                break
+            moves_nw.append(board.get_square_from_pos(
+                (self.x - i, self.y - i)
+            ))
+        output.append(moves_nw)
+        return output
