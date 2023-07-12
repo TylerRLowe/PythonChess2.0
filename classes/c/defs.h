5a2a56276b612e17014dd785d648484072b03e8a
TylerRLowe <trl59@pitt.edu>
2023-07-11 23:22:17 -0400

    Better Bit Board

--- classes/c/defs.h
+++ classes/c/defs.h
@@ -1,99 +1,14 @@
+#define u64 uint64_t
+#define getBit(board,sq) (board & (1ULL << sq))
+#define setBit(board,sq) (board |= 1ULL << sq )
+#define flipBit(board,sq) (board ^= 1ULL << sq )
+#define popBit(board,sq) (board &= ~ (1ULL << sq ))
 
-
-#define WHITE 0x00
-#define BLACK 0x10
-
-#define EMPTY 0x00
-#define PAWN 0x01
-#define KNIGHT 0x02
-#define BISHOP 0x03
-#define ROOK 0x04
-#define QUEEN 0x05
-#define KING 0x06
-
-#define WHITE_PAWN 0x01
-#define WHITE_KNIGHT 0x02
-#define WHITE_BISHOP 0x03
-#define WHITE_ROOK 0x04
-#define WHITE_QUEEN 0x05
-#define WHITE_KING 0x06
-
-#define BLACK_PAWN 0x11
-#define BLACK_KNIGHT 0x12
-#define BLACK_BISHOP 0x13
-#define BLACK_ROOK 0x14
-#define BLACK_QUEEN 0x15
-#define BLACK_KING 0x16
-
-#define PIECE(x) ((x) & 0x0f)
-#define COLOR(x) ((x) & 0x10)
-
-#define CASTLE_ALL 14
-#define CASTLE_WHITE 3
-#define CASTLE_BLACK 11
-#define CASTLE_WHITE_KING 1
-#define CASTLE_WHITE_QUEEN 2
-#define CASTLE_BLACK_KING 4
-#define CASTLE_BLACK_QUEEN 7
-
-#define MATERIAL_PAWN 100
-#define MATERIAL_KNIGHT 320
-#define MATERIAL_BISHOP 330
-#define MATERIAL_ROOK 500
-#define MATERIAL_QUEEN 900
-#define MATERIAL_KING 20000
-
-
-#define RF(rank, file) ((rank) * 8 + (file))
-
-
-
-
-#define RANK_1 0x00000000000000ffL
-#define RANK_2 0x000000000000ff00L
-#define RANK_3 0x0000000000ff0000L
-#define RANK_4 0x00000000ff000000L
-#define RANK_5 0x000000ff00000000L
-#define RANK_6 0x0000ff0000000000L
-#define RANK_7 0x00ff000000000000L
-#define RANK_8 0xff00000000000000L
-
-#define FILE_A 0x0101010101010101L
-#define FILE_B 0x0202020202020202L
-#define FILE_C 0x0404040404040404L
-#define FILE_D 0x0808080808080808L
-#define FILE_E 0x1010101010101010L
-#define FILE_F 0x2020202020202020L
-#define FILE_G 0x4040404040404040L
-#define FILE_H 0x8080808080808080L
-
-typedef unsigned long long bb;
-
-void bb_init();
-void bb_print(bb value);
-bb bb_random();
-
-extern const int POSITION_WHITE_PAWN;
-extern const int POSITION_WHITE_KNIGHT;
-extern const int POSITION_WHITE_BISHOP;
-extern const int POSITION_WHITE_ROOK;
-extern const int POSITION_WHITE_QUEEN;
-extern const int POSITION_WHITE_KING;
-extern const int POSITION_BLACK_PAWN;
-extern const int POSITION_BLACK_KNIGHT;
-extern const int POSITION_BLACK_BISHOP;
-extern const int POSITION_BLACK_ROOK;
-extern const int POSITION_BLACK_QUEEN;
-extern const int POSITION_BLACK_KING;
-
-typedef struct {
-    int squares[64];
-    int color;
-    int castle;
-    int white_material;
-    int black_material;
-    int white_position;
-    int black_position;
-
-} Board;
-
+enum {  a8, b8, c8, d8, e8, f8, g8, h8,
+        a7, b7, c7, d7, e7, f7, g7, h7,
+        a6, b6, c6, d6, e6, f6, g6, h6,
+        a5, b5, c5, d5, e5, f5, g5, h5,
+        a4, b4, c4, d4, e4, f4, g4, h4,
+        a3, b3, c3, d3, e3, f3, g3, h3,
+        a2, b2, c2, d2, e2, f2, g2, h2,
+        a1, b1, c1, d1, e1, f1, g1, h1};
--- classes/c/eval.c
+++ classes/c/eval.c
@@ -1,62 +1,97 @@
-#include "defs.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include "defs.h"
 
-uint64_t whitePawn;
-uint64_t whiteRook;
-uint64_t whiteKnight;
-uint64_t whiteBishop;
-uint64_t whiteKing;
-uint64_t whiteQueen;
-uint64_t blackPawn;
-uint64_t blackKnight;
-uint64_t blackBishop;
-uint64_t blackKing;
-uint64_t blackQueen;
-uint64_t blackRook;
-// making the starting positions
-void initilize(){
-    //bottom right square = bit 0 , top right = bit 63
-    whitePawn = 0xFF00;
-    whiteRook = 0x81;
-    whiteKnight = 0x42;
-    whiteBishop = 0x24;
-    whiteKing = 0x8;
-    whiteQueen = 0x10;
-    blackPawn = whitePawn << 40;
-    blackRook = whiteRook << 56;
-    blackKnight = whiteKnight << 56;
-    blackBishop = whiteBishop << 56;
-    blackKing = whiteKing << 56;
-    blackQueen = whiteQueen << 56;
-}
-int firstPass = 1;
 
-int color;
-uint64_t curr;
-int move(uint64_t board){
-    if(firstPass){
-        firstPass = 0;
-        initilize();
-        curr = whitePawn | whiteRook | whiteKnight | whiteBishop | whiteKing | whiteQueen | blackPawn | blackRook | blackKnight | blackBishop | blackKing | blackQueen;
 
-        //if the board is the same as origanl board, we are white
-        if(board == curr)
-            color = BLACK;
-        else{
-            color = WHITE;
-        }
-    }
-    curr = whitePawn | whiteRook | whiteKnight | whiteBishop | whiteKing | whiteQueen | blackPawn | blackRook | blackKnight | blackBishop | blackKing | blackQueen;
-    printf("Total value: %" PRIx64 "\n%" PRIx64 " ", board , curr);
-    return 0;
-}
+
+void debugging();
 
 int main(){
-    move(4);
+    u64 board = 0;
+    setBit(board,e4);
+    debugging(board);
+
+    
     return 0;
 }
+
+void debugging(u64 board){
+    printf("   A B C D E F G\n");
+    for(int rank = 0; rank < 8; rank++){
+        printf("%d  ", rank);
+        for(int file = 0; file < 8; file++){
+            int sq = rank*8 + file;
+            //printing 1 if sq is ocupied
+            printf("%d ", getBit(board,sq) ? 1 : 0);
+        }
+        printf("\n");
+    }
+}
+// #include "defs.h"
+// #include <stdio.h>
+// #include <stdint.h>
+// #include <inttypes.h>
+
+// uint64_t whitePawn;
+// uint64_t whiteRook;
+// uint64_t whiteKnight;
+// uint64_t whiteBishop;
+// uint64_t whiteKing;
+// uint64_t whiteQueen;
+// uint64_t blackPawn;
+// uint64_t blackKnight;
+// uint64_t blackBishop;
+// uint64_t blackKing;
+// uint64_t blackQueen;
+// uint64_t blackRook;
+// uint64_t squares[64];
+
+// // making the starting positions
+// void initilize(){
+//     //bottom right square = bit 0 , top right = bit 63
+//     whitePawn = 0xFF00;
+//     whiteRook = 0x81;
+//     whiteKnight = 0x42;
+//     whiteBishop = 0x24;
+//     whiteKing = 0x8;
+//     whiteQueen = 0x10;
+//     blackPawn = whitePawn << 40;
+//     blackRook = whiteRook << 56;
+//     blackKnight = whiteKnight << 56;
+//     blackBishop = whiteBishop << 56;
+//     blackKing = whiteKing << 56;
+//     blackQueen = whiteQueen << 56;
+// }
+// int firstPass = 1;
+
+// int color;
+// int castleState;
+// uint64_t curr;
+// int move(uint64_t board){
+//     if(firstPass){
+//         firstPass = 0;
+//         initilize();
+//         curr = whitePawn | whiteRook | whiteKnight | whiteBishop | whiteKing | whiteQueen | blackPawn | blackRook | blackKnight | blackBishop | blackKing | blackQueen;
+
+//         //if the board is the same as origanl board, we are white
+//         if(board != curr)
+//             color = BLACK;
+//         else{
+//             color = WHITE;
+//         }
+//         castleState = CASTLE_ALL;
+//     }
+//     curr = whitePawn | whiteRook | whiteKnight | whiteBishop | whiteKing | whiteQueen | blackPawn | blackRook | blackKnight | blackBishop | blackKing | blackQueen;
+//     printf("Total value: %" PRIx64 "\n%" PRIx64 " ", board , curr);
+//     return 0;
+// }
+
+// int main(){
+//     move(4);
+//     return 0;
+// }
 
 
 
